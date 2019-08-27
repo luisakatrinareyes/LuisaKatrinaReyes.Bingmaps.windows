42beae204ef5072e47123041a70167b4da131c7b
Luisa Katrina Reyes <luisakatrinareyes@gmail.com>
2019-08-27 18:56:36 +0800

    All Files

---
+++ LuisaKatrinaReyes.Bingmaps/LuisaKatrinaReyes.Bingmaps.windows/Infrastructure/helper/Location.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuisaKatrinaReyes.Bingmaps.windows.Infrastructure.helper
+{
+    public class Location
+    {
+        public Location Center { get; set; }
+        public double Heading { get; set; }
+        public double ZoomLevel { get; set; }
+    }
+    
+
+}
