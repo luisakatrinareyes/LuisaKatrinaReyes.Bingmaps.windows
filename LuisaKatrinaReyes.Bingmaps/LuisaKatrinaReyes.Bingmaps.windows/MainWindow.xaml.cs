532ea4faf128b67596861e0923c1775b6c31826c
Luisa Katrina Reyes <luisakatrinareyes@gmail.com>
2019-08-29 18:45:11 +0800

    Double Click Code on Map

--- LuisaKatrinaReyes.Bingmaps/LuisaKatrinaReyes.Bingmaps.windows/MainWindow.xaml.cs
+++ LuisaKatrinaReyes.Bingmaps/LuisaKatrinaReyes.Bingmaps.windows/MainWindow.xaml.cs
@@ -13,13 +13,45 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using RestSharp;
+using Newtonsoft.Json;
 
 
 namespace LuisaKatrinaReyes.Bingmaps.windows
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    public class WeatherArea
+    {
+        public string Lat { get; set; }
+
+        public string Lon { get; set; }
+
+        public Weather[] Weather { get; set; }
+
+        public Main Main { get; set; }
+
+        public Wind Wind { get; set; }
+    }
+
+    public class Weather
+    {
+        public string description { get; set; }
+
+        public string Icon { get; set; }
+    }
+
+    public class Main
+    {
+        public string Temp { get; set; }
+
+        public string Humidity { get; set; }
+
+        public string Pressure { get; set; }
+    }
+
+    public class Wind
+    {
+        public string speed { get; set; }
+    }
 
     public partial class MainWindow : Window
     {
@@ -27,8 +59,35 @@ namespace LuisaKatrinaReyes.Bingmaps.windows
         {
             InitializeComponent();
             KathMap.Focus();
-               //Set the map mode to Aerial with labels
-               KathMap.Mode = new AerialMode(true);
+            KathMap.Mode = new AerialMode(true);
+        }
+
+ 
+        private void KathMap_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var client = new RestClient("https://api.openweathermap.org/data/2.5/weather?q=Dinalupihan&AppID=1b140e3fe06f80d15e36286b073f86ab");
+
+            var request = new RestRequest("", Method.GET);
+
+            IRestResponse response = client.Execute(request);
+
+            var content = response.Content;
+
+            var area = JsonConvert.DeserializeObject<WeatherArea>(content);
+
+            lblDateTime.Content = DateTime.Now.ToString("dd MMMM yyyy hh:mm tt");
+
+            lblSummary.Content = " Description: " + area.Weather[0].description;
+
+            lblTemperature.Content = " Temperature: " + area.Main.Temp;
+
+            lblHumidity.Content = " Humidity: " + area.Main.Humidity;
+
+            lblPressure.Content = " Pressure: " + area.Main.Pressure;
+
+            lblWindspeed.Content = " Windspeed: " + area.Wind.speed;
+
         }
     }
-}
+
+}    
