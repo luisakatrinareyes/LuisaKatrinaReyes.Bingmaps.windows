8212e98a1a67bffaed61f3925a2bfc8e2dea4f71
Luisa Katrina Reyes <luisakatrinareyes@gmail.com>
2019-09-04 17:18:46 +0800

    Adding Pushpin (Current Position)

--- LuisaKatrinaReyes.Bingmaps/LuisaKatrinaReyes.Bingmaps.windows/MainWindow.xaml.cs
+++ LuisaKatrinaReyes.Bingmaps/LuisaKatrinaReyes.Bingmaps.windows/MainWindow.xaml.cs
@@ -55,16 +55,20 @@ namespace LuisaKatrinaReyes.Bingmaps.windows
 
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
             KathMap.Focus();
             KathMap.Mode = new AerialMode(true);
+            KathMap.ViewChangeOnFrame += new EventHandler<MapEventArgs>(MyMap_ViewChangeOnFrame);
+
+
         }
 
- 
         private void KathMap_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
+
             var client = new RestClient("https://api.openweathermap.org/data/2.5/weather?q=Dinalupihan&AppID=1b140e3fe06f80d15e36286b073f86ab");
 
             var request = new RestRequest("", Method.GET);
@@ -87,7 +91,50 @@ namespace LuisaKatrinaReyes.Bingmaps.windows
 
             lblWindspeed.Content = " Windspeed: " + area.Wind.speed;
 
+            e.Handled = true;
+            Point mousePosition = e.GetPosition(this);
+            Location pinLocation = KathMap.ViewportPointToLocation(mousePosition);
+
+            Pushpin pin = new Pushpin();
+            pin.Location = pinLocation;
+            pin.MouseDoubleClick += new MouseButtonEventHandler(KathMap_MouseDoubleClick);
+
+
+            KathMap.Children.Add(pin);
+
+            
+            
+
+        }
+
+        private void KathMap_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+
+            var pushPinContent = 0;
+            var pushPin = sender as Pushpin;
+            if (pushPin != null && pushPin.GetType() == typeof(Pushpin))
+                pushPinContent = Convert.ToInt32(pushPin.Content);
+
+            KathMap.Heading = (double)pushPinContent;
+        }
+
+        private void KathMap_ViewChangeStart(object sender, MapEventArgs e)
+        {
+            KathMap.ViewChangeStart +=
+              new EventHandler<MapEventArgs>(KathMap_ViewChangeStart);
+        }
+
+        void MyMap_ViewChangeOnFrame(object sender, MapEventArgs e)
+        {
+            //Gets the map that raised this event
+            Map map = (Map)sender;
+            //Gets the bounded rectangle for the current frame
+            LocationRect bounds = map.BoundingRectangle;
+            //Update the current latitude and longitude
+            CurrentPosition.Text += String.Format("Northwest: {0:F5}, Southeast: {1:F5} (Current)",
+                        bounds.Northwest, bounds.Southeast);
         }
     }
+}
 
-}    
